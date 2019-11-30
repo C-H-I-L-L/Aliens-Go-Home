be87fa5140203f96756daa9fa12c8b88022ef023
C-H-I-L-L <fartandgohome@gmail.com>
2019-11-29 20:40:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  render() {
+    return (
+      <div className="App">
+        <h1>We will create an awesome game with React, Redux, and SVG!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
