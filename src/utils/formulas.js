4cf8f84dccfb7d227535310c229445478d97f8fd
C-H-I-L-L <fartandgohome@gmail.com>
2019-12-07 02:06:40 -0700

    added flying objects

--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -1,14 +1,14 @@
 export const pathFromBezierCurve = (cubicBezierCurve) => {
-    const {
-      initialAxis, initialControlPoint, endingControlPoint, endingAxis,
-    } = cubicBezierCurve;
-    return `
-      M${initialAxis.x} ${initialAxis.y}
-      c ${initialControlPoint.x} ${initialControlPoint.y}
-      ${endingControlPoint.x} ${endingControlPoint.y}
-      ${endingAxis.x} ${endingAxis.y}
-    `;
-  };
+  const {
+    initialAxis, initialControlPoint, endingControlPoint, endingAxis,
+  } = cubicBezierCurve;
+  return `
+    M${initialAxis.x} ${initialAxis.y}
+    c ${initialControlPoint.x} ${initialControlPoint.y}
+    ${endingControlPoint.x} ${endingControlPoint.y}
+    ${endingAxis.x} ${endingAxis.y}
+  `;
+};
 
 export const radiansToDegrees = radians => ((radians * 180) / Math.PI);
 
@@ -27,14 +27,14 @@ export const calculateAngle = (x1, y1, x2, y2) => {
 };
 
 export const getCanvasPosition = (event) => {
-    // mouse position on auto-scaling canvas
-    // https://stackoverflow.com/a/10298843/1232793
-  
-    const svg = document.getElementById('aliens-go-home-canvas');
-    const point = svg.createSVGPoint();
-  
-    point.x = event.clientX;
-    point.y = event.clientY;
-    const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
-    return {x, y};
+  // mouse position on auto-scaling canvas
+  // https://stackoverflow.com/a/10298843/1232793
+
+  const svg = document.getElementById('aliens-go-home-canvas');
+  const point = svg.createSVGPoint();
+
+  point.x = event.clientX;
+  point.y = event.clientY;
+  const { x, y } = point.matrixTransform(svg.getScreenCTM().inverse());
+  return {x, y};
 };
